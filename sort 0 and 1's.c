aae4ca38fde2acaea7cf8de8065f6d8237089748
Deena Dayalan M <76902858+deena10545@users.noreply.github.com>
2021-03-21 22:38:52 +0530

    Add files via upload

---
+++ sort 0 and 1's.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,a[50],i;
+    printf("enter the no. of array elements:");
+    scanf("%d",&n);
+    printf("Enter array elements:");
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    int left=0,right=n-1;
+    while(left<right)
+    {
+        if(a[left]==0)
+            left++;
+        else if(a[right]==1)
+            right--;
+        else
+        {
+            a[left]=a[left]^a[right];
+            a[right]=a[right]^a[left];
+            a[left]=a[left]^a[right];
+            left++;
+            right--;
+        }
+    }
+    for(i=0;i<n;i++)
+        printf("%d",a[i]);
+return 0;
+}
