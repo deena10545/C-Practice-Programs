695928b039c6f8d4c2143be6fa3f1ff941b85b9a
Deena Dayalan M <76902858+deena10545@users.noreply.github.com>
2021-05-04 12:08:59 +0530

    Update Alternate sorting.c

--- Alternate sorting.c
+++ Alternate sorting.c
@@ -1,24 +1,39 @@
 /*Alternate sorting: Given an array of integers, rearrange the array in such a way that the
 first element is first max and sec element is first min.
 eg. i/p: {1,2,3,4,5,6,7}, o/p:{7,1,6,2,5,3,4} */
-#include <stdio.h>
-#include <stdlib.h>
-
 int main()
 {
-    int a[50],n,i;
-    printf("Enter the no. of array elements:");
+    int n,i,j,a[50];
     scanf("%d",&n);
-    printf("Enter the array elements in sorted order:");
-    for(i=0;i<n;i++)
-        scanf("%d",&a[i]);
-    int small=0,large=n-1;
+    printf("Enter the array elements:");
     for(i=0;i<n;i++)
     {
-        if(i%2!=0)
-            printf("%d ",a[small++]);
-        else
-            printf("%d ",a[large--]);
-    }
-
+    	scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(i%2==1)
+			{
+				if(a[i]>a[j])
+				{
+					a[i]=a[i]^a[j];
+					a[j]=a[j]^a[i];
+					a[i]=a[i]^a[j];
+				}
+			}
+			else
+			{
+				if(a[i]<a[j])
+				{
+					a[i]=a[i]^a[j];
+					a[j]=a[j]^a[i];
+					a[i]=a[i]^a[j];
+				}
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	printf("%d",a[i]);
 }
